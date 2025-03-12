2279b93f79e91a27312866a35a83e3a48a2cab9b
WaLid-Jbir <anawalid737@gmail.com>
2025-03-12 17:39:23 +0000

    Creauin the register layout form + integrating the api for user registration

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm';
 
 const page = () => {
   return (
     <div className='auth-page w-full h-full flex justify-center items-center'>
-      Login page
+      <RegisterForm />
     </div>
   )
 }
--- frontend/context/userContext.js
+++ frontend/context/userContext.js
@@ -1,10 +1,64 @@
+import { useRouter } from 'next/navigation';
 import React, {createContext, useEffect, useState, useContext} from 'react';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 const userContext = React.createContext();
 
 export const UserContextProvider = ({ children }) => {
+
+    const serverUrl = 'http://localhost:8000';
+
+    const router = useRouter();
+
+    const [user, setUser] = useState(null);
+    const [userState, setUserState] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+    const [loading, setLoading] = useState(true);
+
+    // register user
+    const registerUser = async (e) => {
+        e.preventDefault();
+        if(!userState.email.includes('@') || !userState.password || userState.password.length < 6) {
+            toast.error('Please enter a valid email and password with at least 6 characters');
+            return;
+        }
+        try {
+            const res = await axios.post(`${serverUrl}/api/v1/register`, userState);
+            console.log("User registered successfully", res.data);
+            toast.success('You are registered successfully');
+            //clear form
+            setUserState({
+                name: '',
+                email: '',
+                password: '',
+            });
+            router.push('/login');
+        } catch (error) {
+            console.log("Error while registering user", error);
+            toast.error(error.response.data.message);
+        }
+    }
+
+    // dynamic form handler
+    const handlerUserInput = (name) => (e) => {
+        const value = e.target.value;
+        setUserState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
     return (
-        <userContext.Provider value={"Hello from context"}>
+        <userContext.Provider 
+            value={{
+                registerUser,
+                userState,
+                handlerUserInput
+            }}>
             {children}
         </userContext.Provider>
     )
