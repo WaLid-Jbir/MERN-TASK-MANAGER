88255af5efff2691904f1b3ba7a15138ea4e7ee0
WaLid-Jbir <anawalid737@gmail.com>
2025-03-17 19:38:42 +0000

    Get the user tasks

--- backend/src/controllers/task/taskController.js
+++ backend/src/controllers/task/taskController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import Task from '../../models/task/TaskModel.js';
 
+// create task
 export const createTask = asyncHandler(async (req, res) => {
   try {
     const { title, description, dueDate, status, priority } = req.body;
@@ -28,6 +29,19 @@ export const createTask = asyncHandler(async (req, res) => {
     res.status(201).json(task);
 
   } catch (error) {
-    
+    console.log("Error creating task: ", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get tasks
+export const getTasks = asyncHandler(async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const tasks = await Task.find({ user: userId });
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.log("Error getting tasks: ", error);
+    res.status(500).json({ message: error.message });
   }
 });
