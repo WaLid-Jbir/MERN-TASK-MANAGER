85ef91b7ddb8b67d8ab0aa23a71f6b6b1f3caac6
WaLid-Jbir <anawalid737@gmail.com>
2025-03-21 03:39:46 +0000

    Adding the filters buttons on top right of dashboard

--- frontend/hooks/useUserRedirect.tsx
+++ frontend/hooks/useUserRedirect.tsx
@@ -1,27 +1,18 @@
-"use cleint";
+"use client";
 import { useUserContext } from "@/context/userContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const useRedirect = (redirect: string) => {
-  const { userLoginStatus } = useUserContext();
   const router = useRouter();
+  const {user} = useUserContext();
 
   useEffect(() => {
-    const redirectUser = async () => {
-      try {
-        const isLoggedUser = await userLoginStatus();
-
-        if (!isLoggedUser) {
-          router.push(redirect);
-        }
-      } catch (error) {
-        console.log("Error in redirecting User", error);
-      }
-    };
-
-    redirectUser();
-  }, [redirect, userLoginStatus, router]);
+    if (!user || !user.email) {
+      router.push(redirect);
+    }
+    // watch for changes to user, redirect, router
+  }, [user, redirect, router]);
 };
 
 export default useRedirect;
