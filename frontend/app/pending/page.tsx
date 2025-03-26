5bf4338aef85239ee707b005dcc6b6e688023d85
WaLid-Jbir <anawalid737@gmail.com>
2025-03-26 23:43:30 +0000

    Adding pending tasks page

--- frontend/app/pending/page.tsx
+++ frontend/app/pending/page.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+"use client"
+import { useTasks } from "@/context/taskContext";
+import useRedirect from "@/hooks/useUserRedirect";
+import Filters from "../components/filters/Filters";
+import TaskItem from "../components/task-item/TaskItem";
+import { Task } from "@/utils/types";
+import { filteredTasks } from "@/utils/utilities";
+import { useEffect } from "react";
 
-const page = () => {
+export default function Home() {
+
+  useRedirect('/login');
+
+  const {tasks, openModalForAdd, priority, setPriority} = useTasks();
+
+  const pendingTasks = tasks.filter((task: Task) => !task.completed);
+
+  const filtered = filteredTasks(pendingTasks, priority);
+
+  useEffect(() => {
+    setPriority('all')
+  }, []);
+
   return (
-    <div>
-      Pending
-    </div>
-  )
+    <main className="m-6 h-full">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Pending Tasks</h1>
+        <Filters />
+      </div>
+      <div className="pb-[2rem] mt-6 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-[1.5rem]">
+        {filtered.map((task: Task, i: number) => (
+          <TaskItem key={i} task={task} />
+        ))}
+        <button
+          className="h-[16rem] w-full py-2 rounded-md text-lg font-medium text-gray-500 border-dashed border-2 border-gray-400
+          hover:bg-gray-300 hover:border-none transition duration-200 ease-in-out"
+          onClick={openModalForAdd}
+        >
+          Add New Task
+        </button>
+      </div>
+    </main>
+  );
 }
-
-export default page
