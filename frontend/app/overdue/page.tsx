6b11e183edd93403a44798646304d0ede29b5e30
WaLid-Jbir <anawalid737@gmail.com>
2025-03-27 00:03:04 +0000

    Adding overdue tasks page

--- frontend/app/overdue/page.tsx
+++ frontend/app/overdue/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client"
+import { useTasks } from "@/context/taskContext";
+import useRedirect from "@/hooks/useUserRedirect";
+import Filters from "../components/filters/Filters";
+import TaskItem from "../components/task-item/TaskItem";
+import { Task } from "@/utils/types";
+import { filteredTasks } from "@/utils/utilities";
+import { useEffect } from "react";
+import { overdueTasks } from "@/utils/utilities";
 
-const page = () => {
+export default function Home() {
+
+  useRedirect('/login');
+
+  const {tasks, openModalForAdd, priority, setPriority} = useTasks();
+
+  const overdue = overdueTasks(tasks);
+
+  const filtered = filteredTasks(overdue, priority);
+
+  useEffect(() => {
+    setPriority('all')
+  }, []);
+
   return (
-    <div>
-      Overdue
-    </div>
-  )
+    <main className="m-6 h-full">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Overdue Tasks</h1>
+        <Filters />
+      </div>
+      <div className="pb-[2rem] mt-6 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-[1.5rem]">
+        {filtered.map((task: Task, i: number) => (
+          <TaskItem key={i} task={task} />
+        ))}
+        <button
+          className="h-[16rem] w-full py-2 rounded-md text-lg font-medium text-gray-500 border-dashed border-2 border-gray-400
+          hover:bg-gray-300 hover:border-none transition duration-200 ease-in-out"
+          onClick={openModalForAdd}
+        >
+          Add New Task
+        </button>
+      </div>
+    </main>
+  );
 }
-
-export default page
